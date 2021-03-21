07619a528caa60b055f767772c02d2cb5fa73a1a
Bartlomiej Kotarski <bkotarski1@gmail.com>
2021-03-21 14:20:12 +0100

    header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -3,13 +3,18 @@ import React from 'react';
 // components
 import Carousel from '../carousel';
 
+// hooks
+import { useTranslation } from 'react-i18next';
+
 // styles
 import './styles.scss';
 
 const Header: React.FC = () => {
+    const { t } = useTranslation();
+
     return (
         <div className="header--wrapper">
-            <h1>Konwertuj szybko i łatwo pliki bądź kod pomiędzy wybranymi formatami</h1>
+            <h1 className="title">{t('header')}</h1>
             <Carousel />
         </div>
     );
