95ee95ea3f31ab6f2d842c97dd12807ff435bd88
beatinho <beatainez.k@gmail.com>
2021-03-25 21:08:10 +0100

    backend init

---
+++ backend/src/main/java/com/parser/power/PowerApplication.java
@@ -0,0 +1,13 @@
+package com.parser.power;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/parser/power/PowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parser.power;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
