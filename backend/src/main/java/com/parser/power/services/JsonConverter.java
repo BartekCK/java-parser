9fadca9d88b7cb323eba6fe837c9bae54d716cf5
beatinho <beatainez.k@gmail.com>
2021-04-05 23:04:06 +0200

    started json converter xml to json

--- backend/src/main/java/com/parser/power/services/JsonConverter.java
+++ backend/src/main/java/com/parser/power/services/JsonConverter.java
@@ -1,4 +1,108 @@
 package com.parser.power.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
 public class JsonConverter {
+
+    public String convertFromCsvToJson(String csv) {
+        return null;
+    }
+
+    public String convertFromXmlToJson(String xml) throws ParserConfigurationException, IOException, SAXException {
+        //Get Document Builder
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        //Build Document
+        File temp = File.createTempFile("name", ".xml");
+
+        // Delete temp file when program exits.
+        temp.deleteOnExit();
+
+        // Write to temp file
+        BufferedWriter out = new BufferedWriter(new FileWriter(temp));
+        out.write(xml);
+        out.close();
+        Document document = builder.parse(temp);
+
+        //Normalize the XML Structure; It's just too important !!
+        document.getDocumentElement().normalize();
+
+        //Here comes the root node
+        Element root = document.getDocumentElement();
+        System.out.println(root.getNodeName());
+
+        //Get all employees
+        NodeList nList = document.getElementsByTagName(root.getNodeName());
+
+        System.out.println("============================");
+
+        visitChildNodes(nList);
+        return "" ;
+    }
+
+    //This function is called recursively
+    private static void visitChildNodes(NodeList nList) {
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node node = nList.item(temp);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                //Check all attributes
+                if (node.hasAttributes()) {
+                    // get attributes names and values
+                    NamedNodeMap nodeMap = node.getAttributes();
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+                        Node tempNode = nodeMap.item(i);
+                        System.out.println("Attr name : " + tempNode.getNodeName() + "; Value = " + tempNode.getNodeValue());
+                    }
+                    if (node.hasChildNodes()) {
+                        //We got more childs; Let's visit them as well
+                        visitChildNodes(node.getChildNodes());
+                    }
+                }
+                if (node.getNextSibling()==null) {
+                    //We got more childs; Let's visit them as well
+                    System.out.println(node.getNodeName() + ": {");
+                    visitChildNodes(node.getChildNodes());
+                    System.out.println("}");
+                } else
+                    System.out.println("\"" +node.getNodeName() + "\" : \"" + node.getTextContent()+"\"");
+            }
+        }
+    }
+
+    public String convertFromYamlToJson(String yaml) {
+        return null;
+    }
+
+    public String convertFromJsonToCsv(String json) {
+        return null;
+    }
+
+    public String convertFromJsonToXml(String json) {
+        return null;
+    }
+
+    public String convertFromJsonToYaml(String json) {
+        return null;
+    }
+
 }
---
+++ backend/src/test/java/com/parser/power/JsonConverterTest.java
@@ -0,0 +1,39 @@
+package com.parser.power;
+
+import com.parser.power.services.JsonConverter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class JsonConverterTest {
+    @Autowired
+    private JsonConverter jsonConverter;
+
+    @Test
+    void test() throws IOException, SAXException, ParserConfigurationException {
+        String xml = "<book>\n" +
+                "    <title>Some title</title>\n" +
+                "    <description>some description </description>\n" +
+                "    <author>\n" +
+                "        <id>1</id>\n" +
+                "        <name>some author name</name>\n" +
+                "    </author>\n" +
+                "    <review>nice book</review>\n" +
+                "    <review>this book sucks</review>\n" +
+                "    <review>amazing work</review>\n" +
+                "</book>"
+;
+               String result = jsonConverter.convertFromXmlToJson(xml);
+        System.out.println(result);
+    }
+}
