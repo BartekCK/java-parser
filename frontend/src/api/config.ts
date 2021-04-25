5ab7d3a6b1dd3cf905fc0ef87b598787c7474633
Bartlomiej Kotarski <bkotarski1@gmail.com>
2021-04-25 21:18:54 +0200

    production

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
-export const ApiInstance = axios.create({ baseURL: 'http://localhost:8080/api/v1' });
+export const ApiInstance = axios.create({
+    baseURL: process.env.NODE_ENV === 'production' ? 'http://ec2-3-135-249-201.us-east-2.compute.amazonaws.com:8080' : 'http://localhost:8080/api/v1'});
