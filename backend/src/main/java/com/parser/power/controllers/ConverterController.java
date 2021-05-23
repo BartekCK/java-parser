957479ce5370c4e97d39049a7f2480b946ab0bb8
beatinho <beatainez.k@gmail.com>
2021-05-23 13:28:21 +0200

    controller

--- backend/src/main/java/com/parser/power/controllers/ConverterController.java
+++ backend/src/main/java/com/parser/power/controllers/ConverterController.java
@@ -2,24 +2,26 @@ package com.parser.power.controllers;
 
 import com.parser.power.services.JsonConverter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/xml/converter")
+@RequestMapping
 @RequiredArgsConstructor
 public class ConverterController {
 
     private final JsonConverter jsonConverter;
 
-    @PostMapping("/json")
+    @PostMapping("/xml/converter/json")
     public String convertXmlToJson(@RequestBody String xml) throws IOException, SAXException, ParserConfigurationException {
         return jsonConverter.convertFromXmlToJson(xml);
+    }
+
+    @PostMapping("/csv/converter/json/{mainNode}")
+    public String convertCsvToJson(@PathVariable(name = "mainNode") String mainNode, @RequestBody String xml) throws IOException, SAXException, ParserConfigurationException {
+        return jsonConverter.convertFromCsvToJson(mainNode, xml);
     }
 }
