946f81b6d19ee3b33eea9db38357c7e024de4cc2
Bartek <>
2021-06-27 13:41:21 +0200

    Url create start

--- frontend/src/components/editor/Editor.tsx
+++ frontend/src/components/editor/Editor.tsx
@@ -12,18 +12,26 @@ import './styles.scss';
 type Props = {
     selectValue: Parser;
     onChangeType: (event: ChangeEvent<HTMLSelectElement>) => void;
+    text: string;
+    setText?: React.Dispatch<string>;
     readonly?: boolean;
 };
 
 const Editor: React.FC<Props> = (props: Props) => {
-    const { onChangeType, readonly, selectValue } = props;
+    const { onChangeType, readonly, selectValue, text, setText } = props;
+
+    const onChange = (event) => {
+        if (setText) {
+            setText(event.target.value);
+        }
+    };
 
     return (
         <div className="editor--container">
             <div className="header">
                 <Select onChange={onChangeType} value={selectValue} />
             </div>
-            <textarea className="editor" readOnly={readonly} />
+            <textarea value={text} className="editor" readOnly={readonly} onChange={onChange} />
         </div>
     );
 };
